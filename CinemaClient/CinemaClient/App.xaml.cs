5052a5ee61e6943be71d234533367d4cf61e281e
oliwer.kucharzyk <oliwer.kucharzyk@ELPLC-0408>
2023-01-09 18:29:03 +0100

    api client added

---
+++ CinemaClient/CinemaClient/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaClient;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CinemaClient/CinemaClient/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaClient;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
